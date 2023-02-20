fa696b33deb239825d1f94dc10bb8f3baf5df8d3
raffaella <suardini.raffaella@gmail.com>
2023-02-20 17:59:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log(`your current location : ${lat}, ${lng}`);
+  },
+  function () {
+    alert('i could not find your position');
+  }
+);
